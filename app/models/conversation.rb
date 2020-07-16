91bfc26006514ceb3275f6ed1dfb312e5700d17c
sam <sblan45@gmail.com>
2020-07-15 22:28:42 -0400

    modifies conversation controller actions

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,9 @@
 class Conversation < ApplicationRecord
-  validates :sender, :recipient, uniqueness: true
+  validates_uniqueness_of :sender_id, scope: :recipient_id
 
   has_many :messages
 
-  belongs_to :sender, class_name: "User" 
-  belongs_to :recipient, class_name: "User"
+  belongs_to :sender, foreign_key: :sender_id, class_name: "User" 
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
 
 end
