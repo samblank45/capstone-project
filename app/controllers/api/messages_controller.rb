5ecec07947ee685521a918211466384b0382204a
sam <sblan45@gmail.com>
2020-07-27 14:19:06 -0400

    modifies messages create action

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,17 +3,17 @@ class Api::MessagesController < ApplicationController
   before_action :authenticate_user
 
   def create
-    @message = Message.new(
-      user_id: current_user.id,
-      text: params[:text],
-      conversation_id: params[:conversation_id]
-    )
-    @conversation = Conversation.find_by(id: @message.conversation_id)
+    @conversation = Conversation.find_by(id: params[:conversation_id])
     if @conversation.sender_id == current_user.id || @conversation.recipient_id == current_user.id
+      @message = Message.new(
+        user_id: current_user.id,
+        text: params[:text],
+        conversation_id: params[:conversation_id]
+      )
       if @message.save
         render 'show.json.jb'
       else
-        render json: {error: @message.errors.full_messages}, status: 422
+        render json: {error: @message.errors.full_messages}, status: :bad_request
       end
     else 
       render json: {error: @message.errors.full_messages}, status: :forbidden
